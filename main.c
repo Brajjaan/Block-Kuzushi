d2dc0406a8e31f82ea051cac9babcae1d42f99f7
Brajjaan <bryanvouriot@hotmail.com>
2025-01-22 13:18:53 +0100

    Core functions added
    - Created core functionality of the game
    - Includes paddle, ball and blocks.
    - Includes functioning interaction with all mentioned components.
    
    TO DO:
    
    - Add conditions to win or lose the game.

--- main.c
+++ main.c
@@ -1,37 +1,78 @@
 ﻿#include <stdio.h>
 #include "raylib.h"
+#include <math.h>
 
-int main(void)
-{
+int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Block Kuzushi wannabe type game");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    Rectangle paddle = { screenWidth/2 - 50, screenHeight - 30, 100, 20 };
+    Vector2 ball = { paddle.x + paddle.width / 2, paddle.y - 10 };
+    float ballSpeedX = 0.0f, ballSpeedY = -300.0f;
+    const int ballRadius = 10;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    const int blocksPerRow = 10, blockRowCount = 5;
+    const int blockWidth = screenWidth / blocksPerRow - 20;
+    const int blockHeight = 20;
+    Rectangle blocks[blockRowCount][blocksPerRow];
+    bool visible[blockRowCount][blocksPerRow];
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    for (int i = 0; i < blockRowCount; i++) {
+        for (int j = 0; j < blocksPerRow; j++) {
+            blocks[i][j].x = j * (blockWidth + 20) + 10;
+            blocks[i][j].y = i * (blockHeight + 10) + 40;
+            blocks[i][j].width = blockWidth;
+            blocks[i][j].height = blockHeight;
+            visible[i][j] = true;
+        }
+    }
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
         // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        float deltaTime = GetFrameTime();
+        if (IsKeyDown(KEY_LEFT)) paddle.x -= 400 * deltaTime;
+        if (IsKeyDown(KEY_RIGHT)) paddle.x += 400 * deltaTime;
+        if (paddle.x < 0) paddle.x = 0;
+        if (paddle.x + paddle.width > screenWidth) paddle.x = screenWidth - paddle.width;
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        ball.x += ballSpeedX * deltaTime;
+        ball.y += ballSpeedY * deltaTime;
+        if (ball.x >= screenWidth - ballRadius || ball.x <= ballRadius) ballSpeedX *= -1;
+        if (ball.y <= ballRadius) ballSpeedY *= -1;
+        if (ball.y >= screenHeight - ballRadius) ballSpeedY = -fabs(ballSpeedY);
+
+        if (CheckCollisionCircleRec(ball, ballRadius, paddle)) {
+            ballSpeedY = -fabs(ballSpeedY);
+            ballSpeedX = (ball.x - (paddle.x + paddle.width / 2)) / (paddle.width / 2) * 150;
+        }
+
+        for (int i = 0; i < blockRowCount; i++) {
+            for (int j = 0; j < blocksPerRow; j++) {
+                if (visible[i][j] && CheckCollisionCircleRec(ball, ballRadius, blocks[i][j])) {
+                    ballSpeedY = fabs(ballSpeedY);
+                    visible[i][j] = false;
+                    break;
+                }
+            }
+        }
+
         BeginDrawing();
+        ClearBackground(BLACK);
+        DrawRectangleRec(paddle, WHITE);
+        DrawCircleV(ball, ballRadius, WHITE);
 
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        for (int i = 0; i < blockRowCount; i++) {
+            for (int j = 0; j < blocksPerRow; j++) {
+                if (visible[i][j]) DrawRectangleRec(blocks[i][j], YELLOW);
+            }
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+
+    CloseWindow();
     return 0;
 }
