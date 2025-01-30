ba8997e747e7ceae9f6211c1207c92fc39640bd9
Brajjaan <bryanvouriot@hotmail.com>
2025-01-30 15:02:28 +0100

    Losing condition
    - Added a losing condition

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main(void) {
     const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "Block Kuzushi wannabe type game");
 
-    Rectangle paddle = { screenWidth/2 - 50, screenHeight - 30, 100, 20 };
-    Vector2 ball = { paddle.x + paddle.width / 2, paddle.y - 10 };
-    float ballSpeedX = 0.0f, ballSpeedY = -300.0f;
+    Rectangle paddle;
+    Vector2 ball;
+    float ballSpeedX, ballSpeedY;
     const int ballRadius = 10;
 
     const int blocksPerRow = 10, blockRowCount = 5;
@@ -18,55 +18,81 @@ int main(void) {
     Rectangle blocks[blockRowCount][blocksPerRow];
     bool visible[blockRowCount][blocksPerRow];
 
-    for (int i = 0; i < blockRowCount; i++) {
-        for (int j = 0; j < blocksPerRow; j++) {
-            blocks[i][j].x = j * (blockWidth + 20) + 10;
-            blocks[i][j].y = i * (blockHeight + 10) + 40;
-            blocks[i][j].width = blockWidth;
-            blocks[i][j].height = blockHeight;
-            visible[i][j] = true;
+    bool gameActive = true;
+
+    SetTargetFPS(60);
+
+    void InitGameState() {
+        paddle = (Rectangle){ screenWidth/2 - 50, screenHeight - 30, 100, 20 };
+        ball = (Vector2){ paddle.x + paddle.width / 2, paddle.y - 10 };
+        ballSpeedX = 0.0f;
+        ballSpeedY = -300.0f;
+
+        for (int i = 0; i < blockRowCount; i++) {
+            for (int j = 0; j < blocksPerRow; j++) {
+                blocks[i][j].x = j * (blockWidth + 20) + 10;
+                blocks[i][j].y = i * (blockHeight + 10) + 40;
+                blocks[i][j].width = blockWidth;
+                blocks[i][j].height = blockHeight;
+                visible[i][j] = true;
+            }
         }
     }
 
-    SetTargetFPS(60);
+    InitGameState();
 
     while (!WindowShouldClose()) {
         // Update
         float deltaTime = GetFrameTime();
-        if (IsKeyDown(KEY_LEFT)) paddle.x -= 400 * deltaTime;
-        if (IsKeyDown(KEY_RIGHT)) paddle.x += 400 * deltaTime;
-        if (paddle.x < 0) paddle.x = 0;
-        if (paddle.x + paddle.width > screenWidth) paddle.x = screenWidth - paddle.width;
+        if (gameActive) {
+            if (IsKeyDown(KEY_LEFT)) paddle.x -= 400 * deltaTime;
+            if (IsKeyDown(KEY_RIGHT)) paddle.x += 400 * deltaTime;
+            if (paddle.x < 0) paddle.x = 0;
+            if (paddle.x + paddle.width > screenWidth) paddle.x = screenWidth - paddle.width;
 
-        ball.x += ballSpeedX * deltaTime;
-        ball.y += ballSpeedY * deltaTime;
-        if (ball.x >= screenWidth - ballRadius || ball.x <= ballRadius) ballSpeedX *= -1;
-        if (ball.y <= ballRadius) ballSpeedY *= -1;
-        if (ball.y >= screenHeight - ballRadius) ballSpeedY = -fabs(ballSpeedY);
+            ball.x += ballSpeedX * deltaTime;
+            ball.y += ballSpeedY * deltaTime;
+            if (ball.x >= screenWidth - ballRadius || ball.x <= ballRadius) ballSpeedX *= -1;
+            if (ball.y <= ballRadius) ballSpeedY *= -1;
 
-        if (CheckCollisionCircleRec(ball, ballRadius, paddle)) {
-            ballSpeedY = -fabs(ballSpeedY);
-            ballSpeedX = (ball.x - (paddle.x + paddle.width / 2)) / (paddle.width / 2) * 150;
-        }
+            // Conditions
+            if (ball.y >= screenHeight) {
+                gameActive = false;
+            }
 
-        for (int i = 0; i < blockRowCount; i++) {
-            for (int j = 0; j < blocksPerRow; j++) {
-                if (visible[i][j] && CheckCollisionCircleRec(ball, ballRadius, blocks[i][j])) {
-                    ballSpeedY = fabs(ballSpeedY);
-                    visible[i][j] = false;
-                    break;
+            if (gameActive && CheckCollisionCircleRec(ball, ballRadius, paddle)) {
+                ballSpeedY = -fabs(ballSpeedY);
+                ballSpeedX = (ball.x - (paddle.x + paddle.width / 2)) / (paddle.width / 2) * 150;
+            }
+
+            for (int i = 0; i < blockRowCount; i++) {
+                for (int j = 0; j < blocksPerRow; j++) {
+                    if (visible[i][j] && CheckCollisionCircleRec(ball, ballRadius, blocks[i][j])) {
+                        ballSpeedY = fabs(ballSpeedY);
+                        visible[i][j] = false;
+                        break;
+                    }
                 }
+            }
+        } else {
+            DrawText("You lose! Press R to restart", screenWidth / 4, screenHeight / 2, 20, RED);
+            if (IsKeyPressed(KEY_R)) {
+                gameActive = true;
+                InitGameState();
             }
         }
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawRectangleRec(paddle, WHITE);
-        DrawCircleV(ball, ballRadius, WHITE);
 
-        for (int i = 0; i < blockRowCount; i++) {
-            for (int j = 0; j < blocksPerRow; j++) {
-                if (visible[i][j]) DrawRectangleRec(blocks[i][j], YELLOW);
+        if (gameActive) {
+            DrawRectangleRec(paddle, WHITE);
+            DrawCircleV(ball, ballRadius, WHITE);
+
+            for (int i = 0; i < blockRowCount; i++) {
+                for (int j = 0; j < blocksPerRow; j++) {
+                    if (visible[i][j]) DrawRectangleRec(blocks[i][j], YELLOW);
+                }
             }
         }
 
